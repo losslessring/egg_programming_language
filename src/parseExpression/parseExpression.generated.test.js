807d0922795e77019bc24c36a15a34b6c93b812d
Andrey <losslessring@yandex.ru>
2023-10-11 02:30:59 +0300

    Parse expression

--- src/parseExpression/parseExpression.generated.test.js
+++ src/parseExpression/parseExpression.generated.test.js
@@ -4,6 +4,11 @@ describe("Parse Expression", () => {
     it("Parse expression", () => {
         expect(parseExpression).toBeDefined()
     })
-
+    
+    it("Parses string", () => {
+      const string = `"a"`
+      const result = {type: "value", value: "a"}
+      expect(parseExpression(string)).toStrictEqual(result)
+  })
     
 })
--- src/parseExpression/parseExpression.js
+++ src/parseExpression/parseExpression.js
@@ -1,7 +1,25 @@
 import { skipAllSpacesAtTheBeginning } from './../skipAllSpacesAtTheBeginning/skipAllSpacesAtTheBeginning'
 
-export const parseExpression = (string) => {
+export const parseExpression = (programString) => {
 
-    const program = skipAllSpacesAtTheBeginning(string)
+    const program = skipAllSpacesAtTheBeginning(programString)
     
+    let match
+    
+    // console.log(program.match(/^"([^"]*)"/))
+    if(match = /^"([^"]*)"/.exec(program)){
+
+        return {type: "value", value: match[1]}
+    }
+
+    // const matchString = program.match(/^"([^"]*)"/)
+
+    
+    // if(matchString){
+
+    //     return {type: "value", value: matchString[1]}
+    // }
+    
+
+
 }
